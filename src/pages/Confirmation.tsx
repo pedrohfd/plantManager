8457ed6cde1d791d7c3d3256f5220a7727a52c78
Pedro Henrique Franca de Deus <pedrohenriquefrancadedeus@gmail.com>
2021-04-20 16:04:19 -0300

    Aula 02 finalizada

---
+++ src/pages/Confirmation.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+
+import { Button } from '../components/Button'
+
+import colors from '../styles/colors'
+import fonts from '../styles/fonts'
+
+export function Confirmation() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.emoji}>😁</Text>
+
+        <Text style={styles.title}>Prontinho</Text>
+
+        <Text style={styles.subtitle}>
+          Agora vamos começar a cuidar das suas {'\n'} plantinhas com muito
+          cuidado.
+        </Text>
+
+        <View style={styles.footer}>
+          <Button title="Começar" />
+        </View>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    padding: 30
+  },
+  emoji: {
+    fontSize: 78,
+    textAlign: 'center'
+  },
+  title: {
+    fontSize: 24,
+    fontFamily: fonts.heading,
+    textAlign: 'center',
+    color: colors.heading,
+    lineHeight: 32,
+    marginTop: 64
+  },
+  subtitle: {
+    fontSize: 17,
+    fontFamily: fonts.text,
+    textAlign: 'center',
+    lineHeight: 25,
+    marginTop: 14,
+    paddingVertical: 10,
+    color: colors.body_dark
+  },
+  footer: {
+    width: '100%',
+    paddingHorizontal: 50,
+    marginTop: 40
+  }
+})
---
+++ src/pages/UserIdentification.tsx
@@ -0,0 +1,133 @@
+import React, { useState } from 'react'
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard
+} from 'react-native'
+
+import { useNavigation } from '@react-navigation/native'
+
+import { Button } from '../components/Button'
+
+import colors from '../styles/colors'
+import fonts from '../styles/fonts'
+
+export function UserIdentification() {
+  const navigation = useNavigation()
+  const [isFocused, setIsFocused] = useState(false)
+  const [isFilled, setIsFilled] = useState(false)
+  const [name, setName] = useState<string>()
+
+  function handleInputBlur() {
+    setIsFocused(false)
+    setIsFilled(!!name)
+  }
+
+  function handleInputFocus() {
+    setIsFocused(true)
+  }
+
+  function handleInputChange(value: string) {
+    setName(value)
+    setIsFilled(!!value)
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.content}>
+            <View style={styles.form}>
+              <View style={styles.header}>
+                <Text style={styles.emoji}>{isFilled ? '😄' : '😃'}</Text>
+
+                <Text style={styles.title}>
+                  Como podemos {'\n'}
+                  chamar você?
+                </Text>
+              </View>
+
+              <TextInput
+                style={[
+                  styles.input,
+                  (isFocused || isFilled) && { borderColor: colors.green }
+                ]}
+                placeholder="Digite um nome"
+                onBlur={handleInputBlur}
+                onFocus={handleInputFocus}
+                onChangeText={handleInputChange}
+              />
+
+              <View style={styles.footer}>
+                <Button
+                  title="Confirmar"
+                  onPress={() => {
+                    isFilled && navigation.navigate('Confirmation')
+                  }}
+                />
+              </View>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    width: '100%'
+  },
+  content: {
+    flex: 1,
+    width: '100%'
+  },
+  form: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 54
+  },
+  header: {
+    alignItems: 'center'
+  },
+  emoji: {
+    fontSize: 44
+  },
+  title: {
+    fontSize: 24,
+    marginTop: 24,
+    lineHeight: 32,
+    textAlign: 'center',
+    color: colors.heading,
+    fontFamily: fonts.heading
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderColor: colors.gray,
+    color: colors.heading,
+    height: 47,
+    width: '100%',
+    fontSize: 18,
+    marginTop: 50,
+    padding: 10,
+    textAlign: 'center'
+  },
+  footer: {
+    marginTop: 40,
+    width: '100%',
+    paddingHorizontal: 20
+  }
+})
